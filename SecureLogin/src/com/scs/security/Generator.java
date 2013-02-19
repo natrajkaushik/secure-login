c31bc98d33105149b68de6f42ba421e87f281450
Natraj Kaushik <natraj.iitb@gmail.com>
2013-02-19 00:15:28 -0500

    Natraj
    Removed some commented portion of the getHPWD() method.

--- SecureLogin/src/com/scs/security/Generator.java
+++ SecureLogin/src/com/scs/security/Generator.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class Generator {
 	public static final int BIT_LENGTH = 160;
 	public static final BigInteger LARGE_PRIME = getLargePrime(BIT_LENGTH);
+	public static final BigInteger RANDOM_SEED = getRandomInteger(BIT_LENGTH);
 	
 	public static BigInteger getLargePrime(int bitLength){
 		return BigInteger.probablePrime(bitLength, new Random());
@@ -16,12 +17,6 @@ public class Generator {
 	}
 	
 	public static BigInteger getHPWD(){
-		
-		/* BigInteger current = getRandomInteger(BIT_LENGTH);
-		while(current.compareTo(LARGE_PRIME) != -1){
-			current = getRandomInteger(BIT_LENGTH);
-		} */
-		
 		int randomDivisor = (int)((Math.random() * 20) + 5); /* generate a random divisor between 5 and 25 */
 		BigInteger current = getRandomInteger(BIT_LENGTH);
 		current = current.divide(new BigInteger(String.valueOf(randomDivisor)));
